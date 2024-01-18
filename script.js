8ac3bfb647655903a28225a3fb613e08037fe3a9
Serhii Shapovalenko <jackshkiper@gmail.com>
2024-01-18 14:42:09 +0200

    input validation added

--- script.js
+++ script.js
@@ -3,10 +3,24 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highscore = 0;
+const guess = document.querySelector('.guess');
 
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+
+guess.oninput = function () {
+  validateInput(this);
+};
+
+function validateInput(input) {
+  const value = input.value;
+
+  if (value < 1 || value > 20) {
+    input.value = '';
+    displayMessage('⛔ Please enter a number from 1 to 20!');
+  }
+}
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -16,6 +30,7 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.guess').disabled = true;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
